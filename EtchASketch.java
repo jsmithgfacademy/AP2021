734f1221c2c7ec1d31ee40e7f8b8e75ca1e6a369
jsmithmorse <jsmith@gfacademy.org>
2020-10-01 15:41:17 -0400

    Added interactors for text command input

--- EtchASketch.java
+++ EtchASketch.java
@@ -3,6 +3,8 @@ import acm.graphics.*;
 import java.awt.event.*;
 import java.awt.Color;
 import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 /**
  * class EtchASketch creates an
@@ -40,12 +42,23 @@ public class EtchASketch extends GraphicsProgram
     private static int DOWN = 1;
     private static int NOCHANGE = 0;
     private JButton penUp;
+    private JTextField  uiCommand;
     private GTurtle turtle;
+    private JLabel uiMsg;
+    private JButton uiButton;
     
     public void init ()
     {
         penUp = new JButton("Pen Up");
         add(penUp, EAST);
+        JLabel cmdMsg = new JLabel("Enter command >");
+        add(cmdMsg, NORTH);
+        uiCommand = new JTextField(30);
+        add(uiCommand, NORTH);
+        uiMsg = new JLabel("Program status: ");
+        add(uiMsg, SOUTH);
+        uiButton = new JButton("ENTER");
+        add(uiButton, NORTH);
         turtle = new GTurtle();
         double turtleStartX = getGCanvas().getWidth()/2;
         double turtleStartY = getGCanvas().getHeight()/2;
@@ -54,22 +67,43 @@ public class EtchASketch extends GraphicsProgram
         addKeyListeners(this);
     }
     
-    public void actionEvent (ActionEvent aEvent)
+    public void actionPerformed (ActionEvent aEvent)
     {
-        String choice = aEvent.getActionCommand();
+       String choice = aEvent.getActionCommand();
+       displayUImessage(choice);
         if (choice.equals("Pen Up"))
         {
-            
+            displayUImessage("choice");
         }
+        else if (choice.equals("ENTER"))
+        {
+            String commandWord = parseCommand(uiCommand.getText());
+            doCommand(commandWord);
+        }
+  
     }
     
     public void keyPressed (KeyEvent kEvent)
     {
         int keycode = kEvent.getKeyCode();
-        //System.out.println(keycode);
         if (keycode == KeyEvent.VK_UP)
         {
             turtle.move(NOCHANGE, UP);
         }
+    }
+    
+    private String parseCommand(String command)
+    {
+        return "";
+    }
+    
+    private void doCommand(String command)
+    {
+        displayUImessage(command);
+    }
+    
+    private void displayUImessage (String msg)
+    {
+        uiMsg.setText("Program Status: executing command " + msg);
     }
 }
